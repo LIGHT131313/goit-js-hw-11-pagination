ff453f3628421b92af99b5cbac6453ca4986a7b9
LIGHT131313 <131313light@gmail.com>
2023-07-31 13:30:59 +0300

    v1.2 pre_final - add 404 page and pugin

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,10 @@ import { createMarkUp, scrollMore } from './mymodules';
 const refs = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
-  // loadMore: document.querySelector('.load-more'),
   guard: document.querySelector('.js-guard'),
 };
 
 refs.form.addEventListener('submit', onSearch);
-// refs.loadMore.addEventListener('click', onLoadMore);
 
 let page = 1;
 const perPage = 40;
@@ -22,6 +20,12 @@ const simpleOptions = {
   captionsData: 'alt',
   captionDelay: 250,
 };
+const observerOptions = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 0,
+};
+let observer = new IntersectionObserver(onPaginationGallary, observerOptions);
 
 /**
  * Рrocesses the element select and promise
@@ -29,89 +33,70 @@ const simpleOptions = {
  */
 function onSearch(evt) {
   evt.preventDefault();
-  // refs.loadMore.hidden = true;
   searchQuery = refs.form.searchQuery.value;
-  page = 1;
-  refs.gallery.innerHTML = '';
 
-  fetchQuary(searchQuery, page, perPage)
-    .then(({ data }) => {
-      if (!data.hits.length || !searchQuery.trim()) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        refs.gallery.innerHTML = createMarkUp(data.hits);
+  if (!searchQuery.trim()) {
+    Notify.failure('Please enter a non-empty search query');
+  } else {
+    page = 1;
+    refs.gallery.innerHTML = '';
 
-        simpleGallery = new SimpleLightbox('.gallery a', simpleOptions);
+    fetchQuary(searchQuery, page, perPage)
+      .then(({ data }) => {
+        if (!data.hits.length) {
+          Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else {
+          console.log('onSearch', data.totalHits / perPage);
+          console.log('onSearch', page);
+          refs.gallery.innerHTML = createMarkUp(data.hits);
 
-        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+          simpleGallery = new SimpleLightbox('.gallery a', simpleOptions);
 
-        if (data.totalHits / perPage >= page) {
-          // refs.loadMore.hidden = false;
+          Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+          if (data.totalHits / perPage >= page) {
+            observer.observe(refs.guard);
+          }
         }
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
+      })
+      .catch(() => {
+        location.href = './error.html';
+      });
+  }
   evt.target.reset();
 }
 
 /**
- * Рrocesses the element select and promise
+ * Рrocesses pagination and promise
+ * @param {Object} entries
+ * @param {Object} observer
  */
-// function onLoadMore() {
-//   page += 1;
+function onPaginationGallary(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      fetchQuary(searchQuery, page, perPage)
+        .then(({ data }) => {
+          refs.gallery.insertAdjacentHTML('beforeend', createMarkUp(data.hits));
 
-//   fetchQuary(searchQuery, page, perPage)
-//     .then(({ data }) => {
-//       refs.gallery.insertAdjacentHTML('beforeend', createMarkUp(data.hits));
+          simpleGallery.refresh();
 
-//       simpleGallery.refresh();
+          if (data.totalHits / perPage <= page) {
+            console.log('onPaginationGallary', data.totalHits / perPage);
+            console.log('onPaginationGallary', page);
+            observer.unobserve(entry.target);
+            Notify.failure(
+              `We're sorry, but you've reached the end of search results.`
+            );
+          }
 
-//       if (data.totalHits / perPage < page) {
-//         refs.loadMore.hidden = true;
-
-//         Notify.failure(
-//           `We're sorry, but you've reached the end of search results.`
-//         );
-//       }
-
-//       scrollMore(refs.gallery);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// }
-
-let options = {
-  root: null,
-  rootMargin: '300px',
-  threshold: 0,
-};
-
-let observer = new IntersectionObserver(onPaginationGallary, options);
-
-function onPaginationGallary() {
-  fetchQuary(searchQuery, page, perPage)
-    .then(({ data }) => {
-      refs.gallery.insertAdjacentHTML('beforeend', createMarkUp(data.hits));
-
-      simpleGallery.refresh();
-
-      // if (data.totalHits / perPage < page) {
-      //   refs.loadMore.hidden = true;
-
-      //   Notify.failure(
-      //     `We're sorry, but you've reached the end of search results.`
-      //   );
-      // }
-
-      scrollMore(refs.gallery);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+          // scrollMore(refs.gallery);
+        })
+        .catch(() => {
+          location.href = './error.html';
+        });
+    }
+  });
 }
